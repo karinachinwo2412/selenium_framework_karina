3d2941582834dce82873e65ca2d0d1d3b2efbb38
kari2461 <karicwc@gmail.com>
2021-08-11 17:51:10 -0600

    feat: add framework code

--- src/test/java/TestAccount.java
+++ src/test/java/TestAccount.java
@@ -11,20 +11,26 @@ public class TestAccount {
     @Test
     public void Test_Login_Successful(){
         String username = "karina.chinwo.camacho@ucreativa.com";
-            String password = "1234";
+        String password = "1234";
 
-        String pathToDriver =  Main.class.getResource("/chromedriver.exe").getPath();
+        String pathToDriver =  Test.class.getResource("/chromedriver.exe").getPath();
         System.setProperty("webdriver.chrome.driver", pathToDriver);
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.opencart.com/");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-        //------- Go To Login Page
+        //Go To Login Page
         driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a/span[1]")).click();
         driver.findElement(By.linkText("Login")).click();
 
-        //------- Llenar formulario
+        /*
+        EJEMPLO DE LISTAS Y WEBELEMENTS SOLOS
+        WebElement WishList = driver.findElement(By.linkText("Wish List"));
+        ArrayList<> WishListList = driver.findElements(By.linkText("Wish List"));
+        */
+
+        //Llenar formulario
         driver.findElement(By.name("email")).sendKeys(username);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.cssSelector("[value='Login']")).click();
@@ -35,6 +41,7 @@ public class TestAccount {
         driver.close();
         driver.quit();
     }
+
 }
 
  /*
